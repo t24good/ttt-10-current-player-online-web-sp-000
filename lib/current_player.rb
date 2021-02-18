5533b2006b253b425c4a8bf5359c8e9cd9e4e88b
Tanesha Goodman <t24good@icloud.com>
2021-02-18 17:10:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ require 'pry'
 def turn_count(board)
   counter = 0
   board.each do |token|
-    if token = "X" || token == "O"
+    if token == "X" || token == "O"
       counter += 1
       binding.pry
     end
