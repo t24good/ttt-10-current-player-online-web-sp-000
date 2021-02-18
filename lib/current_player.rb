4360a07b31d8515d3b9f149dc2878943a0aa87c5
Tanesha Goodman <t24good@icloud.com>
2021-02-18 17:20:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,3 +9,13 @@ def turn_count(board)
   end
   counter
 end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) % 2 != 0
+    return "0"
+    
+    end
+  end
